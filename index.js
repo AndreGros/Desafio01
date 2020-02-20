90a39e7eda168b738546c8b507b7d9ee8143ba4d
Andre Gros <andre.gros@wssim.com.br>
2020-02-20 10:17:16 -0300

    Adicionado middlewares global e local

--- index.js
+++ index.js
@@ -10,15 +10,40 @@ const projects = [
     }
 ];
 
+/**
+ * Middleware global
+ */
+server.use((req, res, next) => {
+    console.time('Request');
+    console.log(`Metodo chamado: ${req.method}`);
+
+    next();
+
+    console.timeEnd('Request');
+});
+
+/**
+ * Middleware local
+ */
+function checkProjectExists(req, res, next) {
+    const { projectId } = req.params;
+    const project = projects.find(p => p.id == projectId);
+
+    if (!project) {
+        return res.status(404).json({ error: 'Project not found' });
+    }
+
+    req.project = project;
+
+    return next();
+}
+
 server.get('/projects', (req, res) => {
     return res.json(projects);
 })
 
-server.get('/projects/:projectId', (req, res) => {
-    const { projectId } = req.params;
-    const project = projects.find(p => p.id == projectId)
-
-    return res.json(project);
+server.get('/projects/:projectId', checkProjectExists, (req, res) => {
+    return res.json(req.project);
 })
 
 server.post('/projects', (req, res) => {
